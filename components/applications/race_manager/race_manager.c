cd3ad27066ef4eaf61e578256cf002bef11b8a9d
Victor Gomes <victor.drad.g@gmail.com>
2026-04-16 23:48:20 -0300

    feat: new pid values

--- components/applications/race_manager/race_manager.c
+++ components/applications/race_manager/race_manager.c
@@ -22,12 +22,12 @@
 /* ========================================================================== */
 
 #define RACE_MANAGER_REFRESH_RATE_MS                 1
-#define RACE_MANAGER_DEFAULT_STRAIGHTLINE_SPEED_MPS  1.5f
+#define RACE_MANAGER_DEFAULT_STRAIGHTLINE_SPEED_MPS  2.2f
 #define RACE_MANAGER_MAX_ROBOT_SPEED_MPS             4.0f
 
 // Define your different acceleration profiles
 #define RACE_MANAGER_DEFAULT_ROBOT_ACCELERATION_MPS2 8.0f
-#define RACE_MANAGER_SLOW_ACCELERATION_MPS2          4.0f
+#define RACE_MANAGER_SLOW_ACCELERATION_MPS2          3.0f
 
 // Macro function to compute the increment per tick based on the given acceleration
 #define GET_ACCEL_PER_TICK(accel_mps2) \
